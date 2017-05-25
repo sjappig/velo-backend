dc511ce4b7fb963b9111697169d4211c9b6a8f86
Antti Keränen <detegr@gmail.com>
2017-05-25 10:27:37 +0300

    Add 404 route

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,24 @@ extern crate env_logger;
 extern crate chrono;
 extern crate rocket;
 
+use rocket::response::{content, NamedFile};
+use std::path::{Path, PathBuf};
+
 #[get("/players")]
 fn players() -> String {
     "Hello world!".into()
 }
 
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+#[error(404)]
+fn not_found() -> content::HTML<String> {
+    content::HTML(r#"<img src="404.jpg"/>"#.into())
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![players]).launch();
+    rocket::ignite().mount("/", routes![players, files]).catch(errors![not_found]).launch();
 }
