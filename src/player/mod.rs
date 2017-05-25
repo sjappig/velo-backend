879a78481a5496bbf190ff4338aa5a36312e4f1b
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2017-05-25 14:11:52 +0300

    error handling in Player parsing

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -12,22 +12,24 @@ pub struct Player {
 }
 
 impl Player {
-    pub fn parse(tommi_line: &str) -> Player {
+    pub fn parse(tommi_line: &str) -> Result<Player, String> {
         lazy_static! {
             // data from Velo: 64-character alphanumeric id, name
             static ref PARSER_RE: Regex = Regex::new("^([[:alnum:]]{64}),([^,]*)$").unwrap();
         }
 
-        // FIXME: should this have error handling? probably only run once to init db?
-        let captures = PARSER_RE.captures(tommi_line).unwrap();
-        
-        let id = String::from(&captures[1]);
-        let name = String::from(&captures[2]);
+        match PARSER_RE.captures(tommi_line) {
+            Some(captures) => {
+                let id = String::from(&captures[1]);
+                let name = String::from(&captures[2]);
 
-        Player {
-            name: name,
-            id: id,
-            elo: UNDEFINED_ELO
+                Ok(Player {
+                    name: name,
+                    id: id,
+                    elo: UNDEFINED_ELO
+                })
+            },
+            None => Err(format!("Could not parse the player line: {}", tommi_line))
         }
     }
 }
@@ -41,10 +43,18 @@ mod tests {
 
     #[test]
     fn it_parses_a_tommi_line() {
-        let player: Player = Player::parse(TEST_LINE);
+        let player: Player = Player::parse(TEST_LINE).unwrap();
 
         assert_eq!("Ismo", player.name);
         assert_eq!("yBsgFK65Je24kPStpG60mySQAstqtZytURNqUPb8fXbWNTD93tNCMkl2Jhzv7ymy", player.id);
         assert_eq!(UNDEFINED_ELO, player.elo);
+    }
+
+    #[test]
+    fn invalid_line_creates_error() {
+        match Player::parse(&"THIS LINE IS INVALID") {
+            Ok(_) => panic!("Parsing an invalid line should not succeed"),
+            Err(err) => assert!(!err.is_empty())
+        }
     }
 }
